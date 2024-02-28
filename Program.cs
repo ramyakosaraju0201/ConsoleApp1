63873e0073cd63fec2a190638a04ce927ed43724
ramyakosaraju0201 <ramyakosaraju@usf.edu>
2024-02-28 14:36:03 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class BubbleSort
+{
+    static void Main()
+    {
+        int[] array = { 3, 62, 5, 16, 4, 10 };
+
+        Console.WriteLine("Original array: " + string.Join(", ", array));
+
+        // Bubble Sort
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            for (int j = 0; j < array.Length - 1 - i; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    // Swap
+                    (array[j], array[j + 1]) = (array[j + 1], array[j]);
+                }
+            }
+        }
+
+        Console.WriteLine("Sorted array: " + string.Join(", ", array));
+    }
+}

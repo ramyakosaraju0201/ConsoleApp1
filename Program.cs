e66128ecff3f7045d0ed760e89b53b3083442783
ramyakosaraju0201 <ramyakosaraju@usf.edu>
2024-02-28 14:50:06 -0500

    version 2.0

--- Program.cs
+++ Program.cs
@@ -1,25 +1,27 @@
-﻿using System;
-class BubbleSort
+﻿/*
+Author: Ramya Kosaraju
+Date: 2/28/2024
+Comments: This C# Console Application code demonstrates the
+   Bubble Sort Algorithm. No input
+   is required for the script. It will execute without
+   input against two pre-populated arrays.
+*/
+using System;
+int[] array = { 3, 62, 5, 16, 4, 10 };  //input array
+
+Console.WriteLine("Original array: " + string.Join(", ", array)); //printing of input array
+
+// Bubble Sort
+for (int i = 0; i < array.Length - 1; i++)
 {
-    static void Main()
+    for (int j = 0; j < array.Length - 1 - i; j++)
     {
-        int[] array = { 3, 62, 5, 16, 4, 10 };
-
-        Console.WriteLine("Original array: " + string.Join(", ", array));
-
-        // Bubble Sort
-        for (int i = 0; i < array.Length - 1; i++)
+        if (array[j] > array[j + 1])  //comparing numbers
         {
-            for (int j = 0; j < array.Length - 1 - i; j++)
-            {
-                if (array[j] > array[j + 1])
-                {
-                    // Swap
-                    (array[j], array[j + 1]) = (array[j + 1], array[j]);
-                }
-            }
+            // Swapping of numbers
+            (array[j], array[j + 1]) = (array[j + 1], array[j]);
         }
-
-        Console.WriteLine("Sorted array: " + string.Join(", ", array));
     }
 }
+//output is getting printed using below line of code
+Console.WriteLine("Sorted array: " + string.Join(", ", array));
